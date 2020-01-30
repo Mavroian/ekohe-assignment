4d0e532dae0321fd28e37507f89ca341f87cdf31
mavroian <mavflorin@gmail.com>
2020-01-30 22:36:58 +0900

    implemented character list component

---
+++ src/components/character-list/character-list.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Character from "../character/character.component"
+import "./character-list.styles.css"
+
+const CharacterList = ({ characters }) => (
+
+  characters.length !== 0 ?
+    <div className="character-list">
+      {characters.map((character, i) => (
+        <Character key={i} index={i} character={character} />
+      ))}
+    </div>
+    : <h1>Not Found!</h1>
+
+)
+
+export default CharacterList
