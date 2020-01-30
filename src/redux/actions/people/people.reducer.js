9cf3cf82003c890fdd21b102e8625c7efb25f65f
mavroian <mavflorin@gmail.com>
2020-01-30 22:39:32 +0900

    implementing people reducer

---
+++ src/redux/actions/people/people.reducer.js
@@ -0,0 +1,16 @@
+const INITIAL_STATE = {
+  loading: true,
+  people: []
+}
+
+const peopleReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'GET_PEOPLE':
+      return { ...state, loading: true };
+    case 'PEOPLE_RECEIVED':
+      return { ...state, people: action.json, loading: false }
+    default:
+      return state;
+  }
+};
+export default peopleReducer;
