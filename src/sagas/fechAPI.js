02b2f96b6c8426f1ac82972d0eaea83c4d77165f
mavroian <mavflorin@gmail.com>
2020-01-31 00:08:05 +0900

    getting all characters

--- src/sagas/fechAPI.js
+++ src/sagas/fechAPI.js
@@ -1,6 +1,32 @@
+import axios from "axios";
 export const fetchPeople = async () => {
-  const response = await fetch("https://swapi.co/api/people").then(response =>
-    response.json()
-  );
+  let response = [];
+  try {
+    let people = [];
+    response = await axios("https://swapi.co/api/people/")
+      .then(response => {
+        people = response.data.results;
+        return response.data.count;
+      })
+      .then(count => {
+        const numberOfPages = Math.ceil((count - 1) / 10);
+        let promises = [];
+        for (let i = 2; i <= numberOfPages; i++) {
+          promises.push(axios(`https://swapi.co/api/people?page=${i}`));
+        }
+        return Promise.all(promises);
+      })
+      .then(response => {
+        people = response.reduce(
+          (acc, data) => [...acc, ...data.data.results],
+          people
+        );
+        return people;
+      })
+      .catch(error => console.log(error));
+  } catch (error) {
+    console.log(error);
+  }
+
   return response;
 };
