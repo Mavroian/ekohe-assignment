9c4b18b9fdf2ecf6336ef83c658e9f7a1148de91
mavroian <mavflorin@gmail.com>
2020-01-30 22:40:10 +0900

    implementing saga config

---
+++ src/sagas/index.js
@@ -0,0 +1,17 @@
+import { call, put, takeLatest, all } from 'redux-saga/effects';
+import { fetchPeople } from './fechAPI'
+
+
+function* getPeople() {
+  const json = yield call(fetchPeople)
+  yield put({ type: "PEOPLE_RECEIVED", json: json.results, })
+}
+function* actionWatcher() {
+  yield takeLatest('GET_PEOPLE', getPeople)
+}
+
+export default function* rootSaga() {
+  yield all([
+    actionWatcher(),
+  ]);
+}
