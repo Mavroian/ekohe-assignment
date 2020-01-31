e72894d7d6e8bbbc05e8c5978ab76980ddec4343
mavroian <mavflorin@gmail.com>
2020-01-31 21:13:32 +0900

    passing state directly to child component  + using && operator

--- src/components/loading/loading.component.jsx
+++ src/components/loading/loading.component.jsx
@@ -1,18 +1,13 @@
 import React from "react";
-import { connect } from "react-redux";
 import "./loading.styles.css";
 import img from "../../assets/loading_spinner.gif";
 
-const Loading = ({ loading }) =>
-  loading ? (
+const Loading = ({ isLoading }) =>
+  isLoading && (
     <div className="loading">
       <h1>Using The Force</h1>
       <img src={img} alt="loading" />
     </div>
-  ) : null;
+  );
 
-const mapStateToProps = ({ characters: { loading } }) => ({
-  loading: loading
-});
-
-export default connect(mapStateToProps)(Loading);
+export default Loading;
